1ead1aee7e306c831778f5756db5041d40b6fb52
Paul K George <paulkottalil20@gmail.com>
2020-06-24 03:57:37 +0530

    added student model

--- student/models.py
+++ student/models.py
@@ -4,12 +4,12 @@ from django.db import models
 class Student(models.Model):
     name = models.CharField(max_length = 256)
     gender = models.CharField(max_length = 100)
-    age = models.IntergerField(default = 1)
+    age = models.IntegerField(default = 1)
     siblings = models.CharField(max_length = 512)
-    class = models.IntergerField(default = 1)
+    current_class = models.IntegerField(default = 1)
     gpa = models.FloatField(default=3)
 
-    
+
     def __str__(self):
         return self.name
 
